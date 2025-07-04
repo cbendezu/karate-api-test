d4a060cbc5c0ba3379bbc0cefef6e240fe01f320
cbendezu <80866636+cbendezu@users.noreply.github.com>
2025-07-04 03:33:09 +0000

    update

--- src/test/java/api/TestRunner.java
+++ src/test/java/api/TestRunner.java
@@ -5,6 +5,6 @@ import com.intuit.karate.junit5.Karate;
 class TestRunner {
     @Karate.Test
     Karate testAll() {
-        return Karate.run().relativeTo(getClass());
+        return Karate.run("test").relativeTo(getClass());
     }
 }
